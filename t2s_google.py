67b2cc0bcd5ed7bf66d975384415e0444f49f478
Samuel Peláez <samuel.pelaez@neuroelectrics.com>
2023-06-11 00:13:17 +0200

    Create function to convert text to wav

--- t2s_google.py
+++ t2s_google.py
@@ -19,4 +19,27 @@ def list_languages():
     for i, language in enumerate(sorted(languages)):
         print(f"{language:>10}", end="\n" if i % 5 == 4 else "")
         
+
+def text_to_wav(voice_name: str, text: str):
+    language_code = "-".join(voice_name.split("-")[:2])
+    text_input = tts.SynthesisInput(text=text)
+    voice_params = tts.VoiceSelectionParams(
+        language_code=language_code, name=voice_name
+    )
+    audio_config = tts.AudioConfig(audio_encoding=tts.AudioEncoding.LINEAR16)
+
+    client = tts.TextToSpeechClient.from_service_account_file('talker-388916-fcf495d2e4a2.json')
+    #client = tts.TextToSpeechClient()
+    response = client.synthesize_speech(
+        input=text_input,
+        voice=voice_params,
+        audio_config=audio_config,
+    )
+
+    filename = f"{voice_name}.wav"
+    with open(filename, "wb") as out:
+        out.write(response.audio_content)
+        print(f'Generated speech saved to "{filename}"')
+        
 list_languages()
+text_to_wav("en-AU-Neural2-A", "What is the temperature in Sydney?")
