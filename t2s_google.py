ed90ca5628e709289fbb077ddd51e999a6590d77
Samuel Peláez <samuel.pelaez@neuroelectrics.com>
2023-06-11 00:26:28 +0200

    Create function to convert text to mp3

--- t2s_google.py
+++ t2s_google.py
@@ -40,6 +40,40 @@ def text_to_wav(voice_name: str, text: str):
     with open(filename, "wb") as out:
         out.write(response.audio_content)
         print(f'Generated speech saved to "{filename}"')
+
+import time
         
-list_languages()
-text_to_wav("en-AU-Neural2-A", "What is the temperature in Sydney?")
+def text_to_mp3(voice_name: str, text: str):
+    language_code = "-".join(voice_name.split("-")[:2])
+    text_input = tts.SynthesisInput(text=text)
+    voice_params = tts.VoiceSelectionParams(
+        language_code=language_code, name=voice_name
+    )
+    audio_config = tts.AudioConfig(audio_encoding=tts.AudioEncoding.MP3)
+
+    client = tts.TextToSpeechClient.from_service_account_file('talker-388916-fcf495d2e4a2.json')
+    #client = tts.TextToSpeechClient()
+    start_time = time.time()
+    response = client.synthesize_speech(
+        input=text_input,
+        voice=voice_params,
+        audio_config=audio_config,
+    )
+
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+
+    print("Elapsed time: ", elapsed_time)
+
+    filename = f"{voice_name}.mp3"
+    with open(filename, "wb") as out:
+        out.write(response.audio_content)
+        print(f'Generated speech saved to "{filename}"')
+
+
+
+      
+#list_languages()
+#text_to_wav("en-AU-Neural2-A", "What is the temperature in Sydney?")
+text_to_mp3("en-AU-Neural2-A", "What is the temperature in Sydney?")
